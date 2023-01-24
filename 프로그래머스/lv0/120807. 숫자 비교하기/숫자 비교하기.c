55aaba2b2151e4894f780447f8a03e1ade96d005
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2023-01-24 22:59:57 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.52 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    if(num1 == num2){
+        int answer = 1;
+        return answer;
+    }else {
+        int answer = -1;
+        return answer;
+    }
+}
