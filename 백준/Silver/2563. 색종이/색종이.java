88cfef362978f49e0851886558bf7d78dadbc52c
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2023-02-15 13:26:43 +0900

    [Silver V] Title: 색종이, Time: 136 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); //색종이의 수가 주어진다.
+		
+		int[][] paper = new int[100][100]; //100 by 100 인 도화지
+		
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken()); //첫번째 자연수는 x 좌표
+			int y = Integer.parseInt(st.nextToken()); //두번째 자연수는 y 좌표
+			int width = 10; //도형의 크기는 10으로 고정
+			int height = 10;//도형의 크기는 10으로 고정
+			
+			for(int j = x; j < x + 10; j++) {
+				for(int k = y; k < y + 10; k++) {
+					paper[j][k] = i;
+				}
+			}
+		}
+		
+		int count = 0;
+		for(int i = 1; i <= N; i++) {
+			for(int j = 0; j < 100; j++) {
+				for(int k = 0; k < 100; k++) {
+					if(paper[j][k] == i) {
+						count += 1;
+					}
+				}
+			}
+		}
+		System.out.println(count);
+		
+	}
+}
