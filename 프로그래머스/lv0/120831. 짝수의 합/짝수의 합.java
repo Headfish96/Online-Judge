9e08760a39826d505c56dcb6e66cc7ba8ad82624
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2023-04-12 21:33:46 +0900

    [level 0] Title: 짝수의 합, Time: 0.18 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            if(i%2 == 0) sum += i;
+        }
+        System.out.println(sum);
+        int answer = sum;
+        return answer;
+    }
+}
