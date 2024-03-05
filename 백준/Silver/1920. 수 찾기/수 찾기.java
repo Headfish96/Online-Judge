efcf9d8b339fd42e3e3b28e70f8d90a42b79130d
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-03-06 07:37:27 +0900

    [Silver IV] Title: 수 찾기, Time: 2140 ms, Memory: 186512 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,41 +1,53 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int[] arrN;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        arrN = new int[N];
-        for(int i = 0 ; i < arrN.length ; i++) {
-            arrN[i] = Integer.parseInt(st.nextToken());
-        }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arrN = new int[N];
+        for(int i = 0 ; i < N; i++) arrN[i] = sc.nextInt();
+
         Arrays.sort(arrN);
-        
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int[] arrM = new int[M];
-        for(int i = 0 ; i < arrM.length ; i++) {
-            arrM[i] = Integer.parseInt(st.nextToken());
+
+        int M = sc.nextInt();
+
+        for(int i = 0; i < M; i++){
+            if(binarySearch(arrN, sc.nextInt()) >= 0) System.out.println(1);
+            else System.out.println(0);
         }
-        
-        for(int i = 0; i < arrM.length; i++) {
-        	call3(arrM[i]);
+    }
+
+    // 정렬된 arr 배열과 찾고자 하는 key 값이 들어온다.
+    public static int binarySearch(int[] arr, int key) {
+        int lo  = 0;
+        int hi = arr.length - 1;
+
+
+        // 최조에 lo는 0이고 hi는 arr.length-1 이다.
+        // mid 는 lo + hi / 2 이다.
+
+        while(lo <= hi){
+
+            int mid = (lo + hi)/2;
+
+            // 그럼 여기서 key 값이 arr[mid] 보다 크다면 Up인거고
+            // lo는 mid가 되고 기존 mid는 새로 구한다.
+            if(key < arr[mid]){
+                hi = mid - 1;
+            }
+            // 그럼 여기서 key 값이 arr[mid] 보다 작다면 Down인거고
+            // hi는 mid -1이 되고 기존 mid는 새로 구한다.
+            else if (key > arr[mid]) {
+                lo = mid + 1;
+            }
+            // 그럼 여기서 key 값이 arr[mid] 와 같다면 mid를 return하고
+            else return mid;
         }
 
-	}
-	private static void call3(int find) {
-		if(Arrays.binarySearch(arrN, find) >= 0) {
-			System.out.println(1);
-		}else
-			System.out.println(0);
-	}
+        // 그럼 여기서 io와 hi가 같아졌는데도 key 값이 없다면 -1을 return한다.
+        return -1;
+    }
 }
