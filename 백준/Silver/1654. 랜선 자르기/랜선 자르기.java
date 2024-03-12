727aab7678b88ab6cab7cd22f0e3f0e1f4153ad3
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-03-12 22:34:17 +0900

    [Silver II] Title: 랜선 자르기, Time: 168 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -19,6 +19,7 @@ public class Main {
         for(int i = 0; i < lan.length; i++){
             lan[i] = Integer.parseInt(br.readLine());
         }
+        // 자를 수 있는 랜선의 최대 길이를 구하는 함수 Upper 사용
         System.out.println(Upper(lan, N));
     }
 
@@ -27,19 +28,19 @@ public class Main {
     public static long Upper(int[] lan, int N){
         // 랜선들이 길이가 저장되어 있는 배열 lan 중에 가장 큰 랜선의 길이를 저장하는 변수 max 선언
         // 랜선이 길이가 0일 수 없으니 max는 1부터 해도 된다.
-        long max = 0;
+        long max = 1;
         // 배열 lan을 순회하면서 가장 큰 랜선의 길이를 구해 max에 넣는다.
         for(int i = 0; i < lan.length; i++){
             // 입력값 중 제일 큰 값을 저장
             if(max < lan[i]) max = lan[i];
         }
-        max++;
-        // 이분 탐색에서 랜선 길이의 최솟값을 나타내는 변수 low 선언 및 1으로 초기화
-        long min = 0;
-        long mid = 0;
+
+        // 이분 탐색에서 랜선 길이의 최솟값을 나타내는 변수 min 선언 및 0으로 초기화
+        long min = 1;
+        long mid;
 
         // 이분 탐색에서 왼쪽값(최솟값)과 오른쪽값(최댓값)이 같아질때까지 반복
-        while (min < max){
+        while (min <= max){
             mid = (max + min)/2;
             // 중간 값으로 잘랐을때 몇개가 되는지 계산할 변수 count 선언 및 0으로 초기화.
             long count = 0;
@@ -50,7 +51,7 @@ public class Main {
             // 생성된 개수가 원하는 개수 N보다 작음
             if(count < N){
                 // 더 작은 크기로 잘라야 더 많은 개수가 생기니 high의 값을 줄인다.
-                max = mid;
+                max = mid-1;
             }
             // 생성된 개수가 원개는 개수 N보가 크거나 같음
             else if(count >= N){
@@ -60,6 +61,6 @@ public class Main {
                 // low를 x라고 하고 mid를 n이라 하면, x = n+1 = high 이니 high는 n+1이고, x + n + 1 = 2n 이고 따라서 x = n-1이니, n-1, n, n+1 이다.
             }
         }
-        return min-1;
+        return max;
     }
 }
