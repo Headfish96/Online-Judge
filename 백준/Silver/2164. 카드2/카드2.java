cdfa04112b6bbaeded917b7ac7a53f9d8ceb6fbf
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-03-15 22:13:16 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 36640 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,19 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Queue<Integer> q = new ArrayDeque<>();
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		for (int i = 0; i < N; i++) {
-			q.offer(i+1);
-		}
-		while(q.size()!=1) {
-			q.poll();
-			q.offer(q.poll());
-		}
-		System.out.println(q.poll());
-	}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 1; i <= N; i++) deque.addLast(i);
+
+        while (deque.size() > 1){
+//            System.out.println("버릴 수: " + deque.peekFirst());
+            deque.pollFirst(); // 버림
+
+//            System.out.println("뒤에다가 넣을 현재 맨 앞에 있는 수: " + deque.peekFirst());
+            int a = deque.pollFirst();
+            deque.addLast(a);
+        }
+        System.out.println(deque.peek());
+    }
 }
