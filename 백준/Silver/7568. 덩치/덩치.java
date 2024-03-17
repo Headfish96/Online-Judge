80a08971b4c4a4732a2b8690774aceed40f99f2a
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-03-17 20:46:51 +0900

    [Silver V] Title: 덩치, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -2,38 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[][] people;
-	public static void main(String[] args) throws IOException {
-//		System.out.println("helloworld");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		
-		people = new int[N][2];
-		for(int i = 0; i < N; i++) {
-			String[] input = br.readLine().split(" ");
-			people[i][0] = Integer.parseInt(input[0]);
-			people[i][1] = Integer.parseInt(input[1]);
-		}
-		
-//		for(int i = 0; i < N; i++) {
-//			for(int j = 0; j < 2; j++) {
-//				System.out.print(people[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
 
-		for(int i = 0; i < N; i++) {
-			int rank = 1;
-			for(int j = 0; j < N; j++) {
-				if(people[i][0] < people[j][0] && people[i][1] < people[j][1]) rank++;
-			}
-			System.out.print(rank + " ");
-		}
-	}
+        int N = Integer.parseInt(br.readLine());
+        int[][] howBig = new int[N][2];
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            howBig[i][0] = Integer.parseInt(st.nextToken());
+            howBig[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < N; i++){
+            int rank = 0;
+            int weight = howBig[i][0];
+            int height = howBig[i][1];
+            for(int j = 0; j < N; j++){
+                if(i == j) continue;
+                else {
+                    if(weight < howBig[j][0] && height < howBig[j][1]){
+                        rank++;
+                    }
+                }
+            }
+            sb.append(rank+1).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
