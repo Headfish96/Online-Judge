89facd6222df33ede4c2062d7a4f1dd32d076915
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-04-08 02:16:48 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-				
-		Scanner sc = new Scanner(System.in);
-		int[] num_arr;
-		int Max = 0;
-		int maxIndex = 0;
-		num_arr = new int[9];
-		for(int i = 0; i<9; i++) {
-			num_arr[i] = sc.nextInt();
-		}
-		
-		for(int i = 0; i<num_arr.length; i++) {
-			if(num_arr[i] >= Max) {
-				Max = num_arr[i];
-				maxIndex = i;
-			}
-		}
-		System.out.println(Max);
-		System.out.println(maxIndex+1);
-	}
-}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
 
+        int max = Integer.MIN_VALUE;
+        int maxNum = 0;
+
+        for(int i = 0 ; i < 9; i++){
+            int temp = Integer.parseInt(br.readLine());
+            if(max < temp){
+                max = temp;
+                maxNum = i+1;
+            }
+        }
+        sb.append(max).append("\n").append(maxNum);
+        System.out.println(sb);
+    }
+}
