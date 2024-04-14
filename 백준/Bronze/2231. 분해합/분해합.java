70cb30f9360aa5e4f14a64e8e05ed8d560f440d3
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-04-14 13:36:40 +0900

    [Bronze II] Title: 분해합, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,29 +1,27 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		
-		for(int i = 1; i <= N; i++) {
-			int M = i;
-			int sum = 0;
-			while(M != 0) {
-				sum += M%10;
-				M = M/10;
-			}
-			
-			if(sum + i == N) {
-				System.out.println(i);
-				break;
-			}
-			
-			if((sum + i != N) && (i == N)) {
-				System.out.println("0");
-			}
-		}
-	}
-}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
 
+        int N = Integer.parseInt(br.readLine());
+        int count = (int)Math.log10((double)N) + 1;
+        int result = 0;
+        for(int i = N - count*9; i < N; i++){
+            int cnt = (int)Math.log10((double)i) + 1;
+            int temp = 0;
+            for(int j = 1; j <= cnt; j++){
+                temp += (i%((int)Math.pow(10, j))) / (int)Math.pow(10, j-1);
+            }
+            temp += i;
+            if (temp == N){
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
