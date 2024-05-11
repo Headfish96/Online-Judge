7040f76e5bd2a39cfa66f5e7587df1ff08b47043
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-05-12 01:39:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-				
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		for(int i=1; i<=N; i++) {
-			for(int j = 1; j<=N-i; j++) {
-				System.out.print(" ");
-			}
-			for(int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String[] args) throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            String star = ("*").repeat(i);
+            String blank = (" ").repeat(N-i);
+            sb.append(blank).append(star).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
