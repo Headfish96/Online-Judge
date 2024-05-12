4aa38dc1153fbc323d30666ce6031674bc06111e
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-05-12 14:45:37 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14188 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,23 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
+
 public class Main {
-	static final int M = 1234567891;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
-		int L = Integer.parseInt(bw.readLine());
-		String str = new String(bw.readLine());
-		long sum = 0;
-		long pow = 1;
-		char[] arr = str.toCharArray();
-		for(int i = 0; i < L; i++)
-		{
-			sum += (arr[i] - 'a' + 1) * pow % M; //분배법칙
-			pow = pow * 31 % M; //분배법칙
-		}
-		long hash = sum % M;
-		System.out.println(hash);
-	}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        int L = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int r = 31;
+        int M = 1234567891;
+        int[] word = new int[L];
+        int H = 0;
+        for(int i = 0; i < L; i++){
+            word[i] = (int)str.charAt(i) - 96;
+            H += word[i]*Math.pow(31, i);
+        }
+        H = H % M;
+        System.out.println(H);
+    }
 }
