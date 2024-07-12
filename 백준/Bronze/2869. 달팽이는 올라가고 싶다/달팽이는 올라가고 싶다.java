2cb06fd6f194305f17490c8113796a0087be9786
Headfish96 <41338713+Headfish96@users.noreply.github.com>
2024-07-13 03:39:05 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,21 +1,27 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bw.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken()); //자는 동안 내려가는 거리
-		int V = Integer.parseInt(st.nextToken()); //나무 막대 길이
-		if((V - A)%(A-B) == 0) {
-			System.out.println((V- A)/(A-B) + 1);
-		}else
-			System.out.println((V- A)/(A-B) + 2);
-	}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int dis = A - B;
+        // 일단 나무 막대의 길이에서 A를 뺀다.
+        // 그렇게 A를 뺀 남은 만큼얼마나 걸리는지 구하고 하루를 더하면 정답니다.
+
+        if(V == A) System.out.println(1);
+        else if( (V-A) % (A-B) == 0){
+            System.out.println((V-A) / (A-B) + 1);
+        } else {
+            System.out.println((V-A) / (A-B) + 2);
+        }
+    }
 }
